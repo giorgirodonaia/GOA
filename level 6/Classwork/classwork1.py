bb15050c0976526e7b0f0bd4e83e16c9787271ed
giorgirodonaia <giorgirodonaia20@gmail.com>
2024-05-28 11:27:40 -0700

    level 6

---
+++ level 6/Classwork/classwork1.py
@@ -0,0 +1,14 @@
+number1 = int(input())
+number2 = int(input())
+print(number2)
+print(number1+ number2)
+
+
+
+
+firstname = input("please enter you firstname : ")
+lastname = input("please enter you lastname: ")
+age = input("please enter your age: ")
+email = input("please enter your email: ")
+
+print("your information" +firstname + " " + lastname + " " + age + " " + email)
---
+++ level 6/Homework/lesson 6.py
@@ -0,0 +1,32 @@
+#შევქმნათ ცვლადი სახელად num1
+num1 = int(input())
+#შევქმნათ ცვლადი სახელად num2
+num2 = int(input())
+print(num1 + num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 - num2)
+
+#შევქმნათ ცვლადი სახელად user_name
+user_name = input("please enter your name: ")
+print("congratulation, you have entered massage:" + user_name)
+
+
+#შევქმნათ ცვლადი სახელად user_firstname
+user_firstname = input("please enter your name: ")
+#შევქმნათ ცვლადი სახელად user_lastname
+user_lastname = input("please enter you lastname: ")
+#შევქმნათ ცვლადი სახელად user_age
+user_age = input("please enter your age: ")
+
+
+print(user_firstname + " " + user_lastname + " " + user_age + " " + "წლის" )
+      
+      
+
+
+
+
+
+
+
