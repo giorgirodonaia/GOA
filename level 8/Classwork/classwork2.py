e46147a8a9cb4e3dff5227750303eb0c2912f5a8
giorgirodonaia <giorgirodonaia20@gmail.com>
2024-06-06 11:31:19 -0700

    level 8

---
+++ level 8/Classwork/classwork2.py
@@ -0,0 +1,39 @@
+password1 = input("enter password: ")
+password2 = input("enter the same password again: ")
+print(password1 == password2)
+
+
+
+num1 = int(input())
+num2 = int(input())
+print(num1 < num2)
+print(num1 > num2)
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 == num2)
+
+
+
+
+num = int(input("please enter number: " ))
+print(num > 5 and num <= 10)
+
+print(num > 5 or num > 10)
+
+
+print(True or False)
+print(False or True)
+print(True and False)
+print(False and True)
+print(False and False)
+print(True and True)
+
+
+
+
+
+
+
+
+
+
