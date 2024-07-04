ff65b211f81af9544a3fb497afe3c697f17e3e18
giorgirodonaia <giorgirodonaia20@gmail.com>
2024-07-04 13:04:51 -0700

    level 16

---
+++ level 16/Homework/lesson16.py
@@ -0,0 +1,64 @@
+nums = [1, 3, 5, 7, 9]
+print(nums)
+
+nums.pop(4)
+nums.append(11)
+
+print(nums)
+
+numbers = [1, 2, 3, 4]
+print(numbers)
+
+nums.pop(3)
+nums.append(11)
+
+print(numbers)
+
+
+
+fruits = ["orange", "apple", " pineapple"]
+print(fruits)
+
+fruits.pop(2)
+
+print(fruits)
+
+drinks = ["Orange juice", "apple juici", "water"]
+print(drinks)
+
+drinks.pop(2)
+print(drinks)
+drinks.append("water")
+
+print(drinks)
+
+
+
+
+
+
+numbers1 = [1, 2, 3, 4, 5, 6, 7, 5]
+print(numbers1)
+
+print(numbers1.count(5))
+
+cars = ["BMW", "Mercedes", "Ferrari", "Pagani"]
+print(cars)
+print(cars.count("a"))
+
+
+numbers2 = [1, 2, 3, 4, 3, 4, 3, 4, 5]
+sublist = [3, 4]
+print(numbers2.count(sublist))
+
+
+numbers3 = [2, 1, 3, 6, 4, 3, 7, 8, 5, 18]
+print(len(numbers3))
+
+deys_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+print(len(deys_of_the_week))
+
+
+
+telephone = ["iphone", "samsung", "xiaomi"]
+print(len(telephone))
