640b1eae47a3719bca13d31e9c09f5935d620acb
giorgirodonaia <giorgirodonaia20@gmail.com>
2025-02-11 05:54:42 -0800

    commit

---
+++ level 79/Homework/script.js
@@ -0,0 +1,25 @@
+function func1(year){
+    if(((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)){
+        return "Yes"
+    } else {
+        return "No"
+    }
+}
+
+let userInput = prompt("Enter year: ")
+
+console.log(func1(userInput))
+
+
+function sumOfDigit(number){
+    sum = 0
+    nums = String(number)
+    for(let i = 0; i < nums.length; i++){
+        sum += Number(nums[i])
+    }
+    
+    return sum
+}
+
+
+console.log(sumOfDigit(2025))
